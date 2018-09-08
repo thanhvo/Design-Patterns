a7fffe843b24a737c06e5fe533e5bd6d9ed7c09b
Thanh Vo <thanhvv@gmail.com>
2018-09-08 16:42:07 +0700

    Immutable object.

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+
+public class Car {
+	private String model;
+	private String make;
+	private int year;
+	private int milleage;
+	private int VIN;
+	
+	public Car(Car c) {
+		this.model = c.model;
+		this.make = c.make;
+		this.year = c.year;
+		this.milleage = c.milleage;
+		this.VIN = VIN;
+	}
+	
+	public Car clone() {
+		return new Car(this);
+	}
+}
